c22874b8c099dcf937584a512bbeb0993d525e9a
Antonis Skandalis <ant.skandalis@gmail.com>
2020-08-14 12:39:21 +0300

    chore: switch to spa in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,12 +3,15 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'universal',
+  mode: 'spa',
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  generate: {
+    fallback: true,
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
